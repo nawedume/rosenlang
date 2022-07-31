8e4afe6b894d8614517b82b8651eab2452ef374b
Umar N <umar.nawed@gmail.com>
2022-07-31 13:30:29 -0400

    initial commit with distribution code and examples

---
+++ src/Dist.hs
@@ -0,0 +1,53 @@
+{-
+This module holds the definitions of the descrete distributions used for this language.
+-}
+
+module Dist where
+
+import Data.List
+
+data Distribution a = Distribution { dist :: [(a, Double)] }
+
+instance Functor Distribution where
+    fmap func (Distribution dist) =  Distribution { dist = map (\(a, p) -> (func a, p)) dist }
+
+instance Applicative Distribution where
+    pure a = Distribution { dist = [(a, 1.0)]}
+
+    (Distribution funcDist) <*> (Distribution dist) = Distribution {
+        dist = [ (f a, pf * p)  | (f, pf) <- funcDist, (a, p) <- dist ]
+    }
+
+instance Monad Distribution where
+    (Distribution distList) >>= func =
+        Distribution {
+            dist = concat (do
+                (a, p) <- distList
+                let (Distribution newDist) = func a
+                return $ map (\(a, np) -> (a, p * np)) newDist
+            )
+        }
+
+{-
+A function to clean the distribution so that all equal parts are assigned to the same element.
+-}
+clean :: (Ord a, Eq a) => Distribution a -> Distribution a
+clean (Distribution impDist) = Distribution {
+    dist = 
+        let tempDist = groupBy (\(a1, _) (a2, _) -> a1 == a2) $ sort impDist
+            foldProbs xx@((a, _):_) = (a, sum $ map (\(a, p) -> p) xx)
+        in map foldProbs tempDist
+    }
+
+instance (Show a) => Show (Distribution a) where
+    show (Distribution dist) =
+        let first = take 10 dist
+            strList = case (drop 10 dist) of
+                [] -> getListStr first
+                xx -> ("*: " ++ (show $ getOtherP first)):(getListStr first)
+        in foldr (\s1 s2 -> "| " ++ s1 ++ "\n" ++ s2) "----" strList where 
+            getListStr distList = map (\(a, p) -> (show a) ++ ": " ++ (show p)) distList
+            getOtherP first = 1.0 - (sum $ map (\(_, p) -> p) first)
+    
+
+
---
+++ src/Example.hs
@@ -0,0 +1,92 @@
+module Example where
+
+import Dist
+
+-- examples
+
+data Light = Red | Yellow | Green deriving (Show, Eq, Ord)
+data Code = X | Y deriving (Show, Eq, Ord)
+
+lightToCode light = if light == Red then X else Y
+
+
+funcDist :: Distribution (Integer -> Integer)
+funcDist = Distribution {
+    dist = [((+ 1), 0.5), ((+ 2), 0.5)]
+}
+
+intDist = Distribution {
+    dist = [(10, 0.2), (9, 0.8)]
+}
+
+data CoinR = Heads | Tails deriving (Show, Eq, Ord)
+
+coinDist = Distribution [(Heads, 0.5), (Tails, 0.5)]
+
+twoFlips = do
+    c1 <- coinDist
+    c2 <- coinDist
+    return (c1, c2)
+
+
+nFlips :: Int -> Distribution [CoinR]
+nFlips 1 = do
+    c <- coinDist
+    return [c]
+
+nFlips n = do
+    c <- coinDist
+    rest <- nFlips $ n - 1
+    return $ c:rest
+
+lastHead :: Distribution [CoinR]
+lastHead = do
+    c <- coinDist
+    if c == Heads then return $ [c]
+    else do
+        l <- lastHead
+        return $ Tails:l
+
+
+
+light1 :: Distribution Light
+light1  = Distribution { dist = [(Red, 0.45), (Yellow, 0.1), (Green, 0.45)] }
+
+data Action =  Driving | Stopped deriving (Show, Eq, Ord)
+
+cautious_driver :: Light -> Distribution Action
+cautious_driver light = do
+    case light of
+        Red -> Distribution [(Stopped, 1.0)]
+        Yellow -> Distribution [(Stopped, 0.9), (Driving, 0.1)] 
+        Green -> Distribution [(Driving, 1.0)]
+
+aggressive_driver light = do
+    case light of
+        Red -> Distribution [(Stopped, 0.9), (Driving, 0.1)]
+        Yellow -> Distribution [(Stopped, 0.1), (Driving, 0.9)] 
+        Green -> Distribution [(Driving, 1.0)]
+
+
+inverseLight light = case light of
+        Green -> return Red
+        Yellow -> return Red
+        Red -> Distribution [(Green, 0.45 / 0.55), (Yellow, 0.1 / 0.55)]
+
+data CarResult = Crash | Pass deriving (Show, Eq, Ord)
+crash d1 d2 light = clean $ do
+    l1 <- light
+    l2 <- inverseLight l1
+    d1Action <- d1 l1
+    d2Action <- d2 l2
+    if (d1Action == Driving && d2Action == Driving)
+        then Distribution [(Crash, 0.9), (Pass, 0.1)]
+        else return Pass
+
+
+lights = do
+    l1 <- light1
+    l2 <- inverseLight l1
+    return (l1, l2)
+
+
